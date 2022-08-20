a04f8f3790099d9d560975be6550ae042af22d1d
Dirt <clay.nichols@gmail.com>
2022-08-19 23:05:09 -0700

    Finish game start screen, initial dom setup.

--- script.js
+++ script.js
@@ -1,24 +1,74 @@
-function createNewGameScreen() {
+
+const startScreen = (function() {
+    'use strict';
+
     const titlePage = document.createElement("div");
-    titlePage.classList.add("new-game-screen");
-
     const playerImage = document.createElement("img");
-    playerImage.src = "/images/player-icon.svg";
-    playerImage.classList.add("player-icon");
-
     const computerImage = document.createElement("img");
-    computerImage.src = "/images/robot-icon.svg";
-    computerImage.classList.add("robot-icon");
-
     const playerDiv = document.createElement("div");
     const computerDiv = document.createElement("div");
-    playerDiv.classList.add("player-div")
-    computerDiv.classList.add("player-div")
+    const inGame = document.querySelectorAll(".in-game");
+    const whichPlayer = document.querySelector(".which-player");
 
-    playerDiv.append(playerImage);
-    computerDiv.append(computerImage);
-    titlePage.append(playerDiv, computerDiv);
-    document.body.prepend(titlePage);
-}
+    function showStartScreen() {
+    //first set class lists and src for images
+        titlePage.classList.add("new-game-screen");
+        playerImage.src = "/images/player-icon.svg";
+        playerImage.classList.add("player-icon");
+        computerImage.src = "/images/robot-icon.svg";
+        computerImage.classList.add("robot-icon");
+        playerDiv.classList.add("player-div")
+        computerDiv.classList.add("player-div")
+    //everything append/prepend to DOM
+        playerDiv.append(playerImage);
+        computerDiv.append(computerImage);
+        titlePage.append(playerDiv, computerDiv);
+        document.body.prepend(titlePage);
+    }
 
-createNewGameScreen();
+    playerDiv.addEventListener("click", () => {
+        beginGame();
+    })
+
+    computerDiv.addEventListener("click", () => {
+        beginGame();
+    })
+
+    function beginGame() {
+        titlePage.style.display ="none";
+        inGame.forEach((e) => {e.classList.add("visible")});
+        whichPlayer.innerHTML = "Player 1's Turn";
+    }
+
+    return {
+        showStartScreen
+    }
+})();
+
+//gameboard module/object
+const gameBoard = (function() {
+
+    const gameGrid = document.querySelectorAll(".main-grid");
+    const gameBoardGrid = Array.from(gameGrid);
+
+    function placeX() {
+
+    }
+
+    return {
+        gameBoardGrid,
+    }
+})();
+
+//Player factory function
+const Player = (name, symbol) => {
+    const playerName = name;
+    const playerSymbol = symbol;
+};
+
+
+startScreen.showStartScreen();
+
+
+
+
